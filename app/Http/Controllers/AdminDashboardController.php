ab8aa672202ea0a45d0c15ea007921061ae732b5
Mahmudul Hasan <mahmudul006@gmail.com>
2020-10-27 23:52:35 +0600

    updated db query

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -22,24 +22,13 @@ class AdminDashboardController extends Controller
         $start_date = Carbon::now()->format('Y-m-d 00:00:00');
         $end_date = Carbon::now()->format('Y-m-d 23:59:59');
 
-
-        $disease_name = Disease::select('disease_name')
-            ->distinct()
-            ->join('patient_diseases','patient_diseases.disease_id','diseases.id')
-            ->whereBetween('patient_diseases.created_at',[$start_date, $end_date])
-            ->get();
-
         $disease_count = PatientDisease::select('disease_id', DB::raw('count(*) as disease_count'))
             ->groupBy('disease_id')
             ->whereBetween('created_at',[$start_date, $end_date])
             ->get();
 
 
-        $season_name = Season::select('name')
-            ->distinct()
-            ->join('patient_diseases','patient_diseases.season_id','seasons.id')
-            ->whereBetween('patient_diseases.created_at',[$start_date, $end_date])
-            ->get();
+        $season_name = Season::select('name')->distinct()->join('patient_diseases','patient_diseases.season_id','seasons.id')->whereBetween('patient_diseases.created_at',[$start_date, $end_date])->get();
         $season_count =  DB::select(DB::raw("SELECT COUNT(disease_id) as disease_count from patient_diseases GROUP BY season_id"));
 
         /**
@@ -49,18 +38,17 @@ class AdminDashboardController extends Controller
         $me=array();
         $season_count_array = array();
         $season_name_array = array();
-        $i=0;
-        foreach ($disease_name as $disease)
-        {
-            $ne[$i] = $disease->disease_name;
-            $i++;
-        }
+
         $i=0;
         foreach ($disease_count as $disease_coun)
         {
             $me[$i] = $disease_coun->disease_count;
+            $dis_id = $disease_coun->disease_id;
+            $dis_nam= Disease::select('disease_name')->where('id',$dis_id)->first();
+            $ne[$i]= $dis_nam->disease_name;
             $i++;
         }
+
         $i=0;
         foreach ($season_name as $season)
         {
