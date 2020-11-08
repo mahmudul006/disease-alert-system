e2151e314b9a345879739ff5aa0b5fd3a1f1ec8c
Mahmudul Hasan <mahmudul006@gmail.com>
2020-11-08 19:35:55 +0600

    array refactor

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -13,19 +13,18 @@ class AdminDashboardController extends Controller
     //
     public function index()
     {
-
         /**
          * Database query
          */
-        $start_date = Carbon::now()->format('Y-m-d 00:00:00');
-        $end_date = Carbon::now()->format('Y-m-d 23:59:59');
+        $start_date = Carbon::now()->format('Y-m-d 00:00:00' );
+        $end_date = Carbon::now()->format('Y-m-d 23:59:59' );
 
-        $disease_counts = PatientDisease::select('disease_id', DB::raw('count(*) as disease_count'))
+        $disease_counts = PatientDisease::select( 'disease_id', DB::raw('count(*) as disease_count' ))
             ->groupBy('disease_id')
             ->whereBetween('created_at',[$start_date, $end_date])
             ->get();
 
-        $season_name = Season::select('name')->distinct()->join('patient_diseases','patient_diseases.season_id','seasons.id')->get();
+        $season_name = Season::select( 'name' )->distinct()->join('patient_diseases','patient_diseases.season_id','seasons.id')->get();
         $season_counts =  DB::select(DB::raw("SELECT COUNT(disease_id) as disease_count from patient_diseases GROUP BY season_id"));
 
         /**
@@ -36,27 +35,23 @@ class AdminDashboardController extends Controller
         $season_count_array = [];
         $season_name_array = [];
 
-        $i = 0;
+
         foreach ( $disease_counts as $disease_count )
         {
-            $me[$i] = $disease_count->disease_count;
+            $me[] = $disease_count->disease_count;
             $dis_id = $disease_count->disease_id;
             $dis_nam = Disease::select( 'disease_name' )->whereId( $dis_id )->first();
-            $ne[$i] = $dis_nam->disease_name;
-            $i++;
+            $ne[] = $dis_nam->disease_name;
         }
 
-        $i=0;
         foreach ($season_name as $season)
         {
-            $season_name_array[$i] = $season->name;
-            $i++;
+            $season_name_array[] = $season->name;
         }
-        $i=0;
+
         foreach ($season_counts as $season_count)
         {
-            $season_count_array[$i] = $season_count->disease_count;
-            $i++;
+            $season_count_array[] = $season_count->disease_count;
         }
 
         return view('admin.index',compact('me','ne','season_name_array','season_count_array'));
